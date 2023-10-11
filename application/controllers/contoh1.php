ccd562974e39144240ad3dffce43204a047f2c93
hafizhRs31 <adli.zikrilah@gmail.com>
2023-10-10 20:10:22 -0700

    menambahkan file tugas pertemuan 2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1?> Perkenalkan </h1>";
+        echo "Nama Saya Hafizh Rifanulah S
+        Saya Tinggal di Daerah Bekasi Mendesign 
+        Adalah Hobi Saya";
+    }
+}
